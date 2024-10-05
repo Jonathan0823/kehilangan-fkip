d0a7a82c997c1e7ee939adaa44d957b37ecc42c1
Guinn <eguinjonathan08@gmail.com>
2024-10-05 14:59:27 +0700

    Refactor CSS and update page content
    - Refactor CSS in globals.css to remove unused styles and variables
    - Update page content in layout.tsx and page.tsx to reflect the purpose of the application
    - Add appwrite dependency to package.json and configure appwrite client in config.ts

---
+++ app/appwrite/config.ts
@@ -0,0 +1,7 @@
+import { Client, Account, Databases } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`);
