80d49a965cf912043647cd597287506901951017
wignn <tigfiragnafatur1933@gmail.com>
2024-10-06 11:16:15 +0700

    Refactor authentication route: Update Prisma import and remove unnecessary code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,65 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
-import { sql } from "@vercel/postgres";
+import { prisma } from "@/app/lib/prisma";
 
 const handler = NextAuth({
-    session: {
-        strategy: "jwt",
-    },
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: {},
-        email: {},
-        password: {},
+        email: { label: "Email", type: "text", placeholder: "example@example.com" },
+        password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        const response = await sql`
-                    SELECT * FROM users WHERE email = ${credentials?.email}`;
 
-        const user = response.rows[0];
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
 
-        const passwordCorrect = await compare(
-          credentials?.password || " ",
-          user.password
-        );
+          where: {
+            email: credentials?.email,
+          },
+        });
 
-        if (user && passwordCorrect) {
-          return { id: user.id, email: user.email, username: user.username };
+
+        if (user) {
+          const passwordCorrect = await compare(credentials.password, user.password);
+
+          if (passwordCorrect) {
+            return { id: user.id, email: user.email, name: user.name }; 
+          }
         }
         return null;
       },
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
+      }
+      return token;
+    },
+    session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          name: token.name,
+          createdAt: token.createdAt,
+        },
+      };
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
