d122548cadc68cf6a69dc78991e8d1da29530780
wignn <tigfiragnafatur1933@gmail.com>
2024-10-06 17:26:39 +0700

    Refactor API route to find posts and add new API route for fetching post list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['files.edgestore.dev'],
+      },
+
+};
 
 export default nextConfig;
