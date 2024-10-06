85fc0717a63c551372c16d0450ac323a450c480a
Guinn <eguinjonathan08@gmail.com>
2024-10-06 22:38:00 +0700

    Refactor last segment retrieval in reset password page

--- app/(auth)/reset/reset-password/[token]/page.tsx
+++ app/(auth)/reset/reset-password/[token]/page.tsx
@@ -4,7 +4,7 @@ import Reset from "@/app/components/reset-password";
 
 export default function ResetPasswordPage() {
     const pathname = usePathname();
-    const lastSegment = pathname.split("/").pop();
+    const lastSegment = pathname.split("/").pop() || "";
     console.log(lastSegment);
     
     return (
