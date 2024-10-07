7c9df42a5ea4534e59e8205c80cf283639e0ba8a
Guinn <eguinjonathan08@gmail.com>
2024-10-07 19:49:01 +0700

    Refactor CommentBox component to fetch and display comments by post ID

--- app/components/CommentBox.tsx
+++ app/components/CommentBox.tsx
@@ -1,5 +1,6 @@
 "use client"
 import axios from "axios";
+import Image from "next/image";
 import React, { useEffect } from "react";
 
 interface CommentBoxProps {
@@ -9,11 +10,21 @@ interface CommentBoxProps {
   }
 
 const CommentBox = (postid: CommentBoxProps) => {
+  interface Comment {
+    id: string;
+    userimage: string;
+    userName: string;
+    timeAgo: string;
+    content: string;
+  }
+
+  const [comments, setComments] = React.useState<Comment[]>([]);
     useEffect(() => {
         const id = postid.postId;
         const fetchComments = async () => {
           try {
             const response = await axios.get(`/api/comments/${id}`);
+            setComments(response.data);
             console.log("Fetched Comments:", response.data);
           } catch (err) {
             console.error("Fetch error:", err);
@@ -23,8 +34,26 @@ const CommentBox = (postid: CommentBoxProps) => {
         }, [postid]);
           
   return (
-    <div className="bg-white pb-28 p-4 mt-5 items-center justify-center rounded-lg shadow md:max-w-2xl max-w-full w-full">
-      Comments
+    <div className="bg-white pb-28 p-4 mt-5 items-center text-center justify-center rounded-lg shadow md:max-w-2xl max-w-full w-full">
+      Komentar
+      {comments.map((comment) => (
+        <div key={comment.id} className="flex flex-col my-6 w-full ">
+          <div className="flex items-center">
+            <Image
+              src={comment.userimage}
+              width={32}
+              height={32}
+              alt="avatar"
+              className="w-8 h-8 rounded-full"
+            />
+            <div className="ml-2">
+              <div className="text-sm font-semibold text-left">{comment.userName}</div>
+              <div className="text-xs text-gray-500 text-left">{comment.timeAgo}</div>
+            </div>
+          </div>
+          <div className="mt-4 text-left">{comment.content}</div>
+        </div>
+      ))}
     </div>
   );
 };
