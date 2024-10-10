a537e25a2920cce9538bd1dc7d03c1fab9c5b5cb
wignn <tigfiragnafatur1933@gmail.com>
2024-10-10 21:34:45 +0700

    Refactor page.tsx and route.ts files

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -11,6 +11,7 @@ export async function GET(request: Request, {params}: { params: { id: string } }
             where: {
                 postId: String(id),
             },
+            include:{author: true},
         });
         return NextResponse.json(comment);
     } catch (error) {
