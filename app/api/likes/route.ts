6eb5a750a78831d84444af468af62222f28e8fe2
Guinn <eguinjonathan08@gmail.com>
2024-10-11 19:15:35 +0700

    fix when the like data didn't fetched correctly

--- app/api/likes/route.ts
+++ app/api/likes/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
 export async function GET() {
     try{
         const likes = await prisma.like.findMany();
