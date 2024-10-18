1fdae4d518b2c5454663e6485328b8ceb61cff85
wignn <tigfiragnafatur1933@gmail.com>
2024-10-18 14:38:09 +0700

    Refactor getUser route to include posts in the response Add HistoryCard component for displaying history items Add HistoryPage component for displaying user history posts

---
+++ app/history/historyCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface HistoryCardProps {
+  title: string;
+  content: string;
+  date: string;
+  image?: string;
+}
+
+const HistoryCard : React.FC  <HistoryCardProps>=  ({ title, content, date, image }) => {
+    return (
+      <div className="bg-white shadow-md rounded-lg p-4 mb-4">
+        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+        <p className="text-gray-600 mb-2">{content}</p>
+        <span className="text-gray-400 text-sm">{date}</span>
+        {image && (
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-40 object-cover rounded-md mb-2"
+          />
+        )}
+      </div>
+    );
+  };
+  
+  export default HistoryCard;
+  
---
+++ app/history/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import HistoryCard from "./historyCard";
+
+
+const HistoryPage = () => {
+    const { data: session } = useSession();
+    const [posts, setPosts] = useState<{ id: string; title: string; description: string; image: string; date: string }[]>([]);
+    const fecthData = async () => {
+        try{
+            const result = await axios.get(`/api/getUser/${session?.user.id}`)
+            setPosts(result.data.posts)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        if (session?.user.id) {
+            fecthData();
+        }
+    }, [session?.user.id]);
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-6">History Postingan</h1>
+      {posts.map((post) => (
+        <HistoryCard
+          key={post.id}
+          title={post.title}
+          content={post.description}
+          image={post.image}
+          date={post.date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default HistoryPage;
