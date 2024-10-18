e9ad2407bc39064b35c2d35f70199d3b60ff6f7f
wignn <tigfiragnafatur1933@gmail.com>
2024-10-18 16:03:25 +0700

    Refactor historyCard component to include delete and share functionality

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import HistoryCard from "../components/historyCard";
 import Footer from "../components/Footer";
+import Link from "next/link";
 
 const HistoryPage = () => {
   const { data: session } = useSession();
@@ -29,6 +30,27 @@ const HistoryPage = () => {
       fecthData();
     }
   }, [session?.user.id]);
+
+  if (posts.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="bg-white rounded-lg p-6 text-center">
+          <h2 className="text-xl font-bold mb-4">Tidak Ada Postingan</h2>
+          <p className="text-gray-600 mb-4">
+            Saat ini, kamu belum memiliki postingan. Silakan buat postingan baru
+            untuk mulai berbagi!
+          </p>{" "}
+          <Link href="/Laporan">
+            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+              Buat Postingan Baru
+            </button>{" "}
+          </Link>
+        </div>
+        <Footer className="fixed" />
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="min-h-dvh bg-gray-100 p-6">
@@ -41,11 +63,15 @@ const HistoryPage = () => {
             title={post.title}
             content={post.description}
             image={post.image}
+            userId={session?.user.id || ""}
+            postId={post.id}
+            username={session?.user.name || ""}
             date={post.date}
+            fetchPosts={fecthData}
           />
         ))}
       </div>
-      <Footer className="fixed"/>
+      <Footer className="fixed" />
     </div>
   );
 };
